86a0a5bd5675fe19fae8b323707727be8decb158
ikostov91 <i.kostov91@gmail.com>
2020-12-19 23:11:04 +0200

    Engine and Gearbox controllers update.

--- Assets/Scripts/Vehicle/EngineController.cs
+++ Assets/Scripts/Vehicle/EngineController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(IInput))]
 public class EngineController : MonoBehaviour
 {
+    private VehicleController _vehicleController;
+    private GearboxController _gearboxController;
     private IInput _input;
     private IEngine Engine;
     private IGearbox Gearbox;
@@ -12,12 +14,15 @@ public class EngineController : MonoBehaviour
     private float _currentEngineRpm;
     private float _currentEngineTorque = 0f;
     private float _currentBackdriveTorque = 0f;
-    private float _wheelInertia = 0.92f;
+    private const float _wheelInertia = 0.92f;
+    private const float radiansToRevs = 0.159155f;
 
     public float EngineTorque => this._currentEngineTorque;
 
     void Start()
     {
+        this._vehicleController = GetComponent<VehicleController>();
+        this._gearboxController = GetComponent<GearboxController>();
         this._input = GetComponent<IInput>();
         this.Engine = GetComponent<IEngine>();
         this.Gearbox = GetComponent<IGearbox>();
@@ -25,7 +30,6 @@ public class EngineController : MonoBehaviour
         this._currentEngineRpm = this.Engine.MinimumRpm;
     }
 
-    // Update is called once per frame
     void Update()
     {
         this.RevEngine();
@@ -33,61 +37,74 @@ public class EngineController : MonoBehaviour
 
     private void RevEngine()
     {
-        //float currentThrottle = this._input.Throttle;
-        //if (this._currentEngineRpm >= this.Engine.MaximumRmp || cutThrottle)
-        //{
-        //    currentThrottle = 0f;
-        //}
+        float currentThrottle = this._input.Throttle;
+        if (this._currentEngineRpm >= this.Engine.MaximumRmp || this._vehicleController.CutTrottle)
+        {
+            currentThrottle = 0f;
+        }
 
-        //float effInertia = this.Engine.Inertia + this._input.Clutch * (this._wheelInertia * Mathf.Abs(gearRatio));
-        //this._currentBackdriveTorque = 0f;
+        float currentGearRatio = this.Gearbox.ForwardGearRatios[this._gearboxController.Gear];
+        float effInertia = this.Engine.Inertia + this._input.Clutch * (_wheelInertia * Mathf.Abs(currentGearRatio));
+        this._currentBackdriveTorque = 0f;
 
-        //if (gear != 0 && this._input.Clutch == 1)
-        //{
-        //    float wheelRPM = 0f;
-        //    float newRpm = 0f;
-        //    WheelHit hit;
+        if (this._gearboxController.Gear != 0 && this._input.Clutch == 1)
+        {
+            Debug.Log("IN GEAR");
+            float wheelRPM = 0f;
+            float newRpm = 0f;
+            WheelHit hit;
 
-        //    foreach (AxleInfo axle in motorAxels)
-        //    {
-        //        float totalWheelRpm = 0f;
+            foreach (AxleInfo axle in this._vehicleController.MotorAxles)
+            {
+                float totalWheelRpm = 0f;
 
-        //        foreach (WheelCollider wheel in axle.GetAxleWheels())
-        //        {
-        //            if (wheel.GetGroundHit(out hit))
-        //            {
-        //                wheelRPM = GetWheelGroundRPM(wheel) * gearRatio * this.Gearbox.FinalDriveRatio;
-        //                newRpm = this._input.Clutch * this._currentEngineRpm + (1 - this._input.Clutch) * wheelRPM;
-        //                var wheelTorque = (this._currentEngineRpm - newRpm) * this._wheelInertia;
-        //                this._currentBackdriveTorque += wheelTorque;
-        //            }
-        //            totalWheelRpm += wheel.rpm;
-        //        }
+                foreach (WheelCollider wheel in axle.GetAxleWheels())
+                {
+                    if (wheel.GetGroundHit(out hit))
+                    {
+                        wheelRPM = GetWheelGroundRPM(wheel) * currentGearRatio * this.Gearbox.FinalDriveRatio;
+                        newRpm = this._input.Clutch * this._currentEngineRpm + (1 - this._input.Clutch) * wheelRPM;
+                        var wheelTorque = (this._currentEngineRpm - newRpm) * _wheelInertia;
+                        this._currentBackdriveTorque += wheelTorque;
+                    }
+                    totalWheelRpm += wheel.rpm;
+                }
 
-        //        var newWheelRpm = totalWheelRpm / axle.GetAxleWheels().Length;
+                var newWheelRpm = totalWheelRpm / axle.GetAxleWheels().Length;
 
-        //        float calculatedRpm = newWheelRpm * gearRatio * this.Gearbox.FinalDriveRatio;
-        //        this._currentEngineRpm = Mathf.Clamp(calculatedRpm, this.Engine.MinimumRpm, this.Engine.MaximumRmp);
-        //    }
-        //}
+                float calculatedRpm = newWheelRpm * currentGearRatio * this.Gearbox.FinalDriveRatio;
+                this._currentEngineRpm = Mathf.Clamp(calculatedRpm, this.Engine.MinimumRpm, this.Engine.MaximumRmp);
+            }
+        }
 
-        //this._currentBackdriveTorque = Mathf.Clamp(this._currentBackdriveTorque, -1e8f, 1e8f);
+        this._currentBackdriveTorque = Mathf.Clamp(this._currentBackdriveTorque, -1e8f, 1e8f);
 
-        //this._currentEngineRpm = Mathf.Clamp(this._currentEngineRpm, this.Engine.MinimumRpm, this.Engine.MaximumRmp);
+        this._currentEngineRpm = Mathf.Clamp(this._currentEngineRpm, this.Engine.MinimumRpm, this.Engine.MaximumRmp);
 
-        //float momentum = this._currentEngineRpm * effInertia;
-        //momentum += this.GetEngineTorque() * currentThrottle;
-        //momentum -= this._currentBackdriveTorque;
-        //momentum -= this.Engine.Friction * this._currentEngineRpm;
+        float momentum = this._currentEngineRpm * effInertia;
+        momentum += this.GetEngineTorque() * currentThrottle;
+        momentum -= this._currentBackdriveTorque;
+        momentum -= this.Engine.Friction * this._currentEngineRpm;
 
-        //this._currentEngineRpm = momentum / effInertia;
-        //this._currentEngineRpm = Mathf.Clamp(this._currentEngineRpm, this.Engine.MinimumRpm, this.Engine.MaximumRmp);
-        //this._currentEngineTorque = GetEngineTorque() * currentThrottle;
+        this._currentEngineRpm = momentum / effInertia;
+        this._currentEngineRpm = Mathf.Clamp(this._currentEngineRpm, this.Engine.MinimumRpm, this.Engine.MaximumRmp);
+        this._currentEngineTorque = GetEngineTorque() * currentThrottle;
     }
 
     private float GetEngineTorque()
     {
         int roundedRpms = (int)Mathf.Floor(this._currentEngineRpm / 100f) * 100;
         return this.Engine.TorqueCurve[roundedRpms];
+    }
+
+    public float GetWheelGroundRPM(WheelCollider wheel)
+    {
+        WheelHit hit;
+        if (wheel.GetGroundHit(out hit))
+        {
+            float SlipInDirection = hit.forwardSlip / (radiansToRevs * wheel.radius) / radiansToRevs * 60 / Time.deltaTime;
+            return wheel.rpm - SlipInDirection;
+        }
+        else return wheel.rpm;
     }
 }
--- Assets/Scripts/Vehicle/GearboxController.cs
+++ Assets/Scripts/Vehicle/GearboxController.cs
@@ -1,7 +1,6 @@
 ﻿using Assets.Scripts;
 using Assets.Scripts.PlayerInput;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -92,32 +91,4 @@ public class GearboxController : MonoBehaviour
         this._currentGear = gear;
         this._vehicleController.CutTrottle = false;
     }
-}
-
-public class FormulaTempGearbox : MonoBehaviour, IGearbox
-{
-    public int NumberOfGears => 7;
-    public int LowestGear => -1;
-    public int HighestGear => 7;
-    public int NeutralGear => 0;
-    public int ReverseGear => -1;
-    public int FirstGear => 1;
-    public float KPD => 0.96f;
-
-    public Dictionary<int, float> ForwardGearRatios => new Dictionary<int, float>()
-    {
-        { -1, 2.90f },
-        {  0, 0f    },
-        {  1, 2.97f },
-        {  2, 2.07f },
-        {  3, 1.43f },
-        {  4, 1.00f },
-        {  5, 0.71f },
-        {  6, 0.57f },
-        {  7, 0.48f }
-    };
-
-    public float FinalDriveRatio => 3.42f;
-
-    public float ReverseRatio => 2.90f;
 }
